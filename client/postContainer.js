b470c7a80a3d96d54d382daf945fe31a991abae9
Belle <belle@Belles-MacBook-Pro.local>
2019-07-27 21:47:01 -0700

    pulling down now fetchs new messages... but not efficently

--- client/postContainer.js
+++ client/postContainer.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, RefreshControl, ScrollView } from 'react-native';
 import {getAllPosts, setupPostListener} from './backend'
 import Post from './post'
-import {ScrollView} from 'react-native'
 
 
 export default class PostContainer extends Component {
@@ -10,40 +9,32 @@ export default class PostContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      posts: [],
+      refreshing: false,
     }
   }
 
   async componentDidMount() {
-    // console.log('=====================> Post Container')
-    // setupPostListener(this.props.displayName, this.appendPostFromListener)
+    this.fetchData()
+  }
+
+  fetchData = async () => {
     let { data } = await getAllPosts()
     if (data) {
-      this.setState({ posts: data })
-      // this.scrollToBottom()
+      this.setState({ posts: data.reverse() })
     }
   }
 
-  // appendPostFromListener = (post) => {
-  //   console.log(post)
-  //   let { data } = post
-  //   if (!data) { console.error(post, 'no data found'); return }
-  //
-  //   let postData = JSON.parse(JSON.parse(data))
-  //   if (postData) {
-  //     this.setState({ posts: [...this.state.posts, postData] })
-  //     // this.scrollToBottom()
-  //   }
+  _onRefresh = () => {
+    this.setState({refreshing: true});
+    this.fetchData().then(() => {
+      this.setState({refreshing: false});
+    });
   }
-
-  // scrollToBottom = () => {
-  //   this.el.scrollTop = this.el.scrollHeight;
-  // }
-
 
   render() {
     return (
-      <ScrollView>
+      <ScrollView keyboardDismissMode={'on-drag'} refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={this._onRefresh}/>}>
         {this.state.posts.map((post, idx) => {
           return <Post {...post} key={idx} />
         })}
